7821ad5d263f17fe142cc11ba7378b4ccdaedf77
LocalAccount <karina.maria04@yahoo.com>
2024-11-20 20:23:44 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoldovanMariaKarinaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
