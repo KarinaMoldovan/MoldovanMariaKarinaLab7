313f124e8082dbd75bcef69fda484a4533d7776d
LocalAccount <karina.maria04@yahoo.com>
2024-12-07 21:01:45 +0200

    lab9+tema

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -2,7 +2,8 @@ namespace MoldovanMariaKarinaLab7.Models;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
+   
+    public ListPage()
 	{
         InitializeComponent();
     }
@@ -19,4 +20,43 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    
+    private Product _tappedProduct;
+
+    void OnItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        _tappedProduct = e.Item as Product;
+    }
+    async void OnDeleteSelectedItemClicked(object sender, EventArgs e)
+    {
+        if (_tappedProduct != null)
+        {
+            await App.Database.DeleteProductAsync(_tappedProduct);
+            var shopList = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+            _tappedProduct = null;
+            await DisplayAlert("Success", "Product deleted successfully.", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please tap on a product first before deleting.", "OK");
+        }
+    }
+
 }
