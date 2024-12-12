0336f080eea922b4000d43dca3200b72fdb93c0d
LocalAccount <karina.maria04@yahoo.com>
2024-12-12 11:08:57 +0200

    lab10 final(fara tema inca)

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -19,29 +19,34 @@ public partial class ShopPage : ContentPage
    
      async void OnShowMapButtonClicked(object sender, EventArgs e)
              {
-         var shop = (Shop)BindingContext;
-         var address = shop.Adress;
-       //  var locations = await Geocoding.GetLocationsAsync(address);
+                  var shop = (Shop)BindingContext;
+                  var address = shop.Adress;
+                  var locations = await Geocoding.GetLocationsAsync(address);
 
-         var options = new MapLaunchOptions { Name = "Magazinul meuz preferat" };
-        // var shoplocation = locations?.FirstOrDefault();
-        // var myLocation = await Geolocation.GetLocationAsync();
-          var myLocation = new Location(46.7731796289, 23.6213886738);
-        //pentru Windows Machine 
-         var distance = myLocation.CalculateDistance(myLocation,  DistanceUnits.Kilometers);
-      if (distance < 5)
-         {
-         var request = new NotificationRequest
-                 {
-             Title = "Ai de facut cumparaturi in apropiere!",
-             Description = address,
-                 Schedule = new NotificationRequestSchedule
-         {
-         NotifyTime = DateTime.Now.AddSeconds(1)
-         }
-     };
-          LocalNotificationCenter.Current.Show(request);
-     }
-         await Map.OpenAsync(myLocation, options);
-        }
+                  var options = new MapLaunchOptions
+                  { 
+                      Name = "Magazinul meu preferat" 
+                  };
+                  var shoplocation = locations?.FirstOrDefault();
+                    /* var shoplocation= new Location(46.7492379, 23.5745597);//pentru
+                   Windows Machine */
+                  var myLocation = await Geolocation.GetLocationAsync();
+                     // var myLocation = new Location(46.7731796289, 23.6213886738);
+                    //pentru Windows Machine 
+                  var distance = myLocation.CalculateDistance(shoplocation,  DistanceUnits.Kilometers);
+                      if (distance < 5)
+                         {
+                             var request = new NotificationRequest
+                                 {
+                                     Title = "Ai de facut cumparaturi in apropiere!",
+                                     Description = address,
+                                      Schedule = new NotificationRequestSchedule
+                                         {
+                                                NotifyTime = DateTime.Now.AddSeconds(1)
+                                         }
+                                 };
+                                   LocalNotificationCenter.Current.Show(request);
+                           }
+                       await Map.OpenAsync(shoplocation, options);
+                      }
 }
